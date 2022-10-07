e3c6080b803199cff73604e657309b8237625ec8
David Parker <dlparker1005@gmail.com>
2022-10-07 15:48:34 -0400

    Adding MMPU compatibility

--- templates/levels.php
+++ templates/levels.php
@@ -160,7 +160,7 @@ function pmpro_advanced_levels_shortcode($atts, $content=null, $code="")
 		<?php if(!empty($back_link)) { ?>
 		<nav id="nav-below" class="navigation" role="navigation">
 			<div class="nav-previous alignleft">
-				<?php if(!empty($current_user->membership_level->ID)) { ?>
+				<?php if ( pmpro_hasMembershipLevel() ) { ?>
 					<a href="<?php echo pmpro_url("account")?>"><?php _e('&larr; Return to Your Account', 'pmpro-advanced-levels-shortcode');?></a>
 				<?php } elseif(!is_front_page()) { ?>
 					<a href="<?php echo home_url()?>"><?php _e('&larr; Return to Home', 'pmpro-advanced-levels-shortcode');?></a>
